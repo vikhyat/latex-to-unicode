0aed7c7dcd2599909c7e0af3d5a4c9a20a1a8b29
Vikhyat Korrapati <vikhyatk@gmail.com>
2011-06-13 12:22:58 +0530

    added \cal

--- convert.rb
+++ convert.rb
@@ -50,6 +50,7 @@ module LatexToUnicode
     result = apply_modifier(result, "_", LatexToUnicode::SUBSCRIPTS)
     result = apply_modifier(result, "\\bb", LatexToUnicode::BB)
     result = apply_modifier(result, "\\bf", LatexToUnicode::BF)
+    result = apply_modifier(result, "\\cal", LatexToUnicode::CAL)
     
     result
   end
