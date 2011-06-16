3bedd29e521b697de37e5b7677a13ddbffde7f82
Vikhyat Korrapati <vikhyatk@gmail.com>
2011-06-16 16:48:30 +0530

    added preprocess method.

--- lib/convert.rb
+++ lib/convert.rb
@@ -3,7 +3,10 @@ require File.dirname(__FILE__) + '/data.rb'
 require File.dirname(__FILE__) + '/latex_grammar.rb'
 
 module LatexToUnicode
+  def self.preprocess(str)
+    translate(str.gsub(/\s/, ''), SYMBOLS)
+  end
   def self.convert(str)
-    LatexParser.new.parse(translate(str.gsub(/\s/, ''), SYMBOLS)).value
+    LatexParser.new.parse(preprocess(str)).value
   end
 end
