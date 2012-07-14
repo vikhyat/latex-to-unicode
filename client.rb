ac0d9a7cd324b9c173fa914024aaf1e1dfe5ad90
Vikhyat Korrapati <vikhyatk@gmail.com>
2012-07-14 14:19:11 +0530

    removed the batch processing mode.

--- client.rb
+++ client.rb
@@ -1,46 +1,4 @@
 # coding: utf-8
 require File.dirname(__FILE__) + '/lib/convert.rb'
 
-def compare(rem, pr)
-  pr.split('').each.with_index do |x, i|
-    return false if x != rem[i]
-  end
-  return true
-end
-
-def replace(input, pair1, pair2)
-  output  = []
-  rem     = input.split('')
-  in_flag = false
-  buffer  = []
-  while rem.length > 0
-    if in_flag
-      if compare(rem[0], pair2)
-        rem.shift(2)
-        in_flag = false
-        output.push LatexToUnicode::convert(buffer.join)
-        buffer = []
-      else
-        buffer.push rem.shift
-      end
-    else
-      if compare(rem, pair1)
-        rem.shift(2)
-        in_flag = true
-      else
-        output.push rem.shift
-      end
-    end
-  end
-  output.join
-end
-
-if ARGV[0] == "-f"
-  input   = File.read(ARGV[1])
-  output  = replace(input, '\\(', '\\)')
-  output  = replace(output, "$$", "$$")
-  output  = replace(output, "$", "$")
-  puts output
-else
-  puts LatexToUnicode::convert(gets.chomp)
-end
+puts LatexToUnicode::convert(gets.chomp)
